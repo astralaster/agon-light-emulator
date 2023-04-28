b56db63c97e6711b935ea5b56875d77a29d538d6
Sascha Schneider <schneider.sascha@mein.gmx>
2023-04-28 23:41:42 +0200

    Added SDL, shows Window, prints ASCII to terminal.

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,80 @@
-fn main() {
-    println!("Agon VDP SDL started.");
+extern crate sdl2;
 
-    let ports = serialport::available_ports().expect("No ports found!");
-    for p in ports {
-        println!("{}", p.port_name);
-    }
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use serialport::SerialPort;
+use std::time::Duration;
 
-    use core::time::Duration;
-
-    let mut port = serialport::new("/dev/ttyUSB0", 1152_000)
-    .timeout(Duration::from_millis(10))
-    .open().expect("Failed to open port");
-
+pub fn read_serial(mut port : Box<dyn SerialPort>) -> Option<u8>
+{
     let mut serial_buf: Vec<u8> = vec![0; 1];
-
-    println!("Starting read loop.");
-    loop
-    {
+    let mut read_bytes = 0;
         match port.read(serial_buf.as_mut_slice())
         {
-            Ok(n) =>
+            Ok(n) => return Some(serial_buf[0]),
+            Err(_e) => return None,
+        }
+}
+
+pub fn main() -> Result<(), String> {
+
+
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+
+    let window = video_subsystem
+        .window("rust-sdl2 demo: Video", 800, 600)
+        .position_centered()
+        .opengl()
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
+
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
+    canvas.present();
+    let mut event_pump = sdl_context.event_pump()?;
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+
+        canvas.clear();
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
+        // The rest of the game loop goes here...
+        // Serial
+        let port = serialport::new("/dev/ttyUSB0", 1152_000)
+        .timeout(Duration::from_millis(10))
+        .open().expect("Failed to open port");
+  
+        // let ports = serialport::available_ports().expect("No ports found!");
+        // for p in ports {
+        //     println!("{}", p.port_name);
+        // }
+
+        //println!("Read from serial.");
+        match read_serial(port)
+        {
+            Some(n) => match n
             {
-                //println!("Received: {:#02x}", serial_buf[0]);
-                match serial_buf[0] {
-                    0x08 => println!("Cursor Left"),
-                    n @ _ => println!("Unknown Command {:#02x} received!", n),
-                }
-            },
-            Err(e) => (), //println!("Error: {}", e),
+                n if n >= 0x20 && n != 0x7F => println!("{}", n as char),
+                0x08 => println!("Cursor Left"),
+                _n => println!("Unknown Command {:#02x} received!", n),
+            }
+            None => (),
         }
     }
 
-    println!("Bye!");
+    Ok(())
 }
