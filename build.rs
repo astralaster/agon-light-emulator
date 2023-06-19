7392f2b879656b876e4856ea664e7708d50cffc0
Sascha Schneider <schneider.sascha@mein.gmx>
2023-06-19 22:28:43 +0200

    Add tag and fallback to get_git_hash in build.rs.

--- build.rs
+++ build.rs
@@ -1,12 +1,28 @@
 fn get_git_hash() -> String {
-    let output = std::process::Command::new("git")
-        .args(&["rev-parse", "--short", "HEAD"])
-        .output()
-        .expect("Failed to execute git command.");
-
-    let git_hash = String::from_utf8(output.stdout).expect("Failed to convert git output to string.");
-
-    git_hash
+    let output_tag = std::process::Command::new("git").args(&["tag", "--points-at", "HEAD"]).output();
+    
+    if output_tag.is_ok() {
+        let tag = output_tag.unwrap();
+        if tag.status.success() {
+            let git_tag = String::from_utf8(tag.stdout).expect("Failed to convert git output to string.");
+            if git_tag.len() > 0 {
+                return git_tag;
+            }
+        }
+    }    
+    
+    let output_hash = std::process::Command::new("git").args(&["rev-parse", "--short", "HEAD"]).output();
+    if output_hash.is_ok() {
+        let hash = output_hash.unwrap();
+        if hash.status.success() {
+            let git_hash = String::from_utf8(hash.stdout).expect("Failed to convert git output to string.");
+            if git_hash.len() > 0 {
+                return git_hash;
+            }
+        }
+    }
+    
+    return String::from("unknown"); // Failed to get
 }
 
 fn main() {
